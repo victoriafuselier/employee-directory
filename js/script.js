70f887b2fe6c98e17cf57526d673533446396583
Victoria Fuselier <vic.fuselier@gmail.com>
2023-07-09 17:59:39 -0500

    Cleaned up folder structure and misc improvements
    Deleted old placeholder photo.
    
    Moved CSS files into a CSS folder and updated links in HTML. Normalize CSS was in a parent folder and was told it needed to be in my repo.
    
    Fixed URL for API in script.js. There were spaces in my URL!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
-email, location, phone, dob &noinfo &nat=US`;
+email,location,phone,dob&noinfo&nat=US`;
 const gridContainer = document.querySelector('.grid-container');
 const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal-content');
